5d133fab53c2bd0c75799336415b6e28ccda13ad
1264527381 <38730371+1264527381@users.noreply.github.com>
2018-04-30 13:12:41 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,39 +1,5 @@
-#!/bin/bash
-echo "欢迎使用一键搭建"
-echo "即将搭建的是4.30版本"
-echo "准备开始安装"
-read -p "回车后开始安装"
-echo "请输入你的内网ip" 
-read -p "内网ip： " ip
-cp /root/PUBG_XiNian_02/restart.sh /root/restart.sh
-chmod +x restart.sh
-wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
-chmod +x shadowsocks-all.sh
-./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
-
-
-echo "ss搭建成，请记住连接信息"
-read -p "记住了吗？任意键继续" 
-
-curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
-source ~/.bash_profile
-nvm install v9.8.0
-nvm alias default v9.8.0
-yum -y install gcc-c++
-yum -y install flex
-yum -y install bison
-wget http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
-tar -zxvf libpcap-1.8.1.tar.gz
-cd libpcap-1.8.1
-./configure
-make
-make install
-
-git clone https://github.com/1264527381/PUBG_XiNian_02.git
-cd PUBG_XiNian_02/
-npm i
-npm i -g pino
-npm install -g forever
-forever start index.js sniff eth0 $ip | pino
-
-echo "搭建完成"
+#!binbashecho 欢迎使用一键搭建echo 即将搭建的是4.30版本echo 准备开始安装read -p 回车后开始安装echo 请输入你的内网ip read -p 内网ip：  ipcp rootPUBG_XiNian_02restart.sh rootrestart.shchmod +x restart.shwget --no-check-certificate -O shadowsocks-all.sh httpsraw.githubusercontent.comteddysunshadowsocks_installmastershadowsocks-all.shchmod +x shadowsocks-all.sh.shadowsocks-all.sh 2&1  tee shadowsocks-all.log
+echo ss搭建成，请记住连接信息read -p 记住了吗？任意键继续 
+curl httpsraw.githubusercontent.comcreationixnvmv0.13.1install.sh  bashsource ~.bash_profilenvm install v9.8.0nvm alias default v9.8.0yum -y install gcc-c++yum -y install flexyum -y install bisonwget httpwww.tcpdump.orgreleaselibpcap-1.8.1.tar.gztar -zxvf libpcap-1.8.1.tar.gzcd libpcap-1.8.1.configuremakemake install
+git clone httpsgithub.com1264527381PUBG_XiNian_02.gitcd PUBG_XiNian_02npm inpm i -g pinonpm install -g foreverforever start index.js sniff eth0 $ip  pino
+echo 搭建完成
